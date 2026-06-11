a3bd90ea3861e69f7557ae2e9bc2a450fb542686
Abhi-205 <abhayjadhav205@gmail.com>
2026-06-12 00:18:01 +0530

    integrate model with FastAPI

--- ML_Model_with_FastAPI/main.py
+++ ML_Model_with_FastAPI/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel , Field , computed_field
 from typing import Literal , Annotated
 import pickle
 import pandas as pd
 
 #import the model 
-with open("pickle_model_path.pkl") as f:
+with open("model_path.pkl" , "rb") as f:
     model = pickle.load(f)
 
 app = FastAPI()
@@ -18,20 +19,7 @@ class UserInput(BaseModel):
     income_lpa : Annotated[float , Field(... , gt = 0 , description='Annual income of the user')]
     smoker : Annotated[bool , Field(... , description='is user a smoker?')]
     city : Annotated[str ,Field(description='The city user belongs to') ]
-    occuption :Annotated[Literal[ 'retired',     'freelancer',        'student', 'government_job','business_owner',     'unemployed',    'private_job'] ,Field(description='Occuption of the user') ]
-
-    tier_1_cities = [
-    "Mumbai", "Delhi", "Bangalore", "Chennai", "Kolkata", "Hyderabad", "Pune"]
-
-    tier_2_cities = ["Jaipur", "Chandigarh", "Indore", "Lucknow", "Patna", "Ranchi",
-    "Visakhapatnam", "Coimbatore", "Bhopal", "Nagpur", "Vadodara", "Surat",
-    "Rajkot", "Jodhpur", "Raipur", "Amritsar", "Varanasi", "Agra",
-    "Dehradun", "Mysore", "Jabalpur", "Guwahati", "Thiruvananthapuram",
-    "Ludhiana", "Nashik", "Allahabad", "Udaipur", "Aurangabad", "Hubli",
-    "Belgaum", "Salem", "Vijayawada", "Tiruchirappalli", "Bhavnagar",
-    "Gwalior", "Dhanbad", "Bareilly", "Aligarh", "Gaya", "Kozhikode",
-    "Warangal", "Kolhapur", "Bilaspur", "Jalandhar", "Noida", "Guntur",
-    "Asansol", "Siliguri"]
+    occuption :Annotated[Literal[ 'retired',     'freelancer',  'student', 'government_job','business_owner',     'unemployed',    'private_job'] ,Field(description='Occuption of the user') ]
 
     @computed_field
     @property
@@ -41,12 +29,12 @@ class UserInput(BaseModel):
     @computed_field
     @property
     def lifestyle_risk(self)->str:
-        if self.smoker['smoker'] == 'True' and self.smoker['bmi'] > 30:
+        if self.smoker == 'True' and self.bmi > 30:
             return 'high'
-        elif self.smoker['smoker'] == 'True' and self.smoker['bmi'] > 27:
+        elif self.smoker == 'True' and self.bmi > 27:
             return 'mdeium'
         else :
-            return 'low'
+         return 'low'
 
 
     @computed_field
@@ -62,10 +50,43 @@ class UserInput(BaseModel):
     @computed_field
     @property
     def city_tier(self) -> int:
-        if self.city in self.tier_1_cities:
+        tier_1_cities = [
+            "Mumbai", "Delhi", "Bangalore", "Chennai", "Kolkata", "Hyderabad", "Pune"]
+
+        tier_2_cities = ["Jaipur", "Chandigarh", "Indore", "Lucknow", "Patna", "Ranchi",
+        "Visakhapatnam", "Coimbatore", "Bhopal", "Nagpur", "Vadodara", "Surat",
+        "Rajkot", "Jodhpur", "Raipur", "Amritsar", "Varanasi", "Agra",
+        "Dehradun", "Mysore", "Jabalpur", "Guwahati", "Thiruvananthapuram",
+        "Ludhiana", "Nashik", "Allahabad", "Udaipur", "Aurangabad", "Hubli",
+        "Belgaum", "Salem", "Vijayawada", "Tiruchirappalli", "Bhavnagar",
+        "Gwalior", "Dhanbad", "Bareilly", "Aligarh", "Gaya", "Kozhikode",
+        "Warangal", "Kolhapur", "Bilaspur", "Jalandhar", "Noida", "Guntur",
+        "Asansol", "Siliguri"]
+        if self.city in tier_1_cities:
             return 1
-        elif self.city in self.tier_2_cities:
+        elif self.city in tier_2_cities:
             return 2
         else:
             return 3
     
+@app.post('/predict')
+def predict_premium(data : UserInput):
+
+   data =  pd.DataFrame([{
+        'bmi' : data.bmi ,
+        'age_group' : data.age_group,
+        'lifestyle_risk' : data.lifestyle_risk , 
+        'city_tier' : data.city_tier , 
+        'income_lpa' : data.income_lpa ,
+        'occupation' : data.occuption
+
+    }])
+
+   prediction = model.predict(data)[0]
+
+   return JSONResponse(
+       status_code=200 , 
+       content={
+           'predicted_category' : prediction
+       }
+   )
